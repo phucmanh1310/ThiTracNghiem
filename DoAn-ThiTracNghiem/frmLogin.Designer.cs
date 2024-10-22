b8a6a566c593239abaa9bc75410d744a03260a81
phucmanh1310 <phucmanhtran08@gmail.com>
2024-10-22 15:55:38 +0700

    ditme tui nay
    thằng vinh thằng thắng

---
+++ DoAn-ThiTracNghiem/frmLogin.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace DoAn_ThiTracNghiem
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitle = new Label();
+            lblUsername = new Label();
+            lblPassword = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            btnSignUp = new Button();
+            btnThoat = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = Color.RoyalBlue;
+            lblTitle.Location = new Point(67, 33);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(227, 46);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "ĐĂNG NHẬP";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblUsername.ForeColor = Color.RoyalBlue;
+            lblUsername.Location = new Point(48, 99);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(128, 23);
+            lblUsername.TabIndex = 1;
+            lblUsername.Text = "Tên đăng nhập";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPassword.Location = new Point(48, 169);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(86, 23);
+            lblPassword.TabIndex = 2;
+            lblPassword.Text = "Mật khẩu";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(54, 125);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(250, 27);
+            txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(54, 195);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(253, 27);
+            txtPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.RoyalBlue;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.Font = new Font("Segoe UI", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(57, 252);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(237, 40);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "ĐĂNG NHẬP";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnSignUp
+            // 
+            btnSignUp.BackColor = Color.White;
+            btnSignUp.FlatAppearance.BorderSize = 0;
+            btnSignUp.Font = new Font("Segoe UI", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSignUp.ForeColor = Color.RoyalBlue;
+            btnSignUp.Location = new Point(57, 298);
+            btnSignUp.Name = "btnSignUp";
+            btnSignUp.Size = new Size(237, 35);
+            btnSignUp.TabIndex = 6;
+            btnSignUp.Text = "ĐĂNG KÝ";
+            btnSignUp.UseVisualStyleBackColor = false;
+            // 
+            // btnThoat
+            // 
+            btnThoat.BackColor = Color.MidnightBlue;
+            btnThoat.ForeColor = Color.White;
+            btnThoat.Location = new Point(213, 339);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(94, 29);
+            btnThoat.TabIndex = 7;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = false;
+            btnThoat.Click += this.btnThoat_Click;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(358, 394);
+            Controls.Add(btnThoat);
+            Controls.Add(btnSignUp);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Controls.Add(lblTitle);
+            Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForeColor = Color.RoyalBlue;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Đăng Nhập";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitle;
+        private Label lblUsername;
+        private Label lblPassword;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Button btnSignUp;
+        private Button btnThoat;
+    }
+}
---
+++ DoAn-ThiTracNghiem/frmLogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace DoAn_ThiTracNghiem
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn chắc chắn muốn thoát chương trình chứ?", "Thoát chương trình?", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                Dispose();
+            }
+
+        }
+    }
+}
